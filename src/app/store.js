3b1a52e49c72646fd6d7f87369ad486018c1ed21
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-17 18:25:39 +0600

    create thunk and slices for redux crud

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import transactionReducer from "../features/transation/transactionSlice";
 
 export const store = configureStore({
-    reducer: {},
+	reducer: {
+		transaction: transactionReducer,
+	},
 });
