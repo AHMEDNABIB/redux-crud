bebd0958f0d364ff40f572ccb153e04bb19e48c6
AHMEDNABIB <nabibpallab@gmail.com>
2024-05-21 11:19:38 +0600

    change the program to rtk query format

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import transactionReducer from "../features/transation/transactionSlice";
+import { apiSlice } from "../features/api/apiSlice";
+
 
 export const store = configureStore({
 	reducer: {
-		transaction: transactionReducer,
+		[apiSlice.reducerPath]: apiSlice.reducer,
 	},
+	middleware: (getDefaultMiddlewares) => {
+		getDefaultMiddlewares().concat(apiSlice.middleware)
+	}
 });
